c84f5ddc14179804f000976e01962bfb289031cd
chan-alex <chan.k.leong@gmail.com>
2018-03-26 12:24:12 +0800

    before Biting the Bullet: Adjusting Our Models

--- lists/views.py
+++ lists/views.py
@@ -4,9 +4,9 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+    #if request.method == 'POST':
+    #    Item.objects.create(text=request.POST['item_text'])
+    #    return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
@@ -15,4 +15,5 @@ def view_list(request):
     return render(request, 'list.html', {'items':items})
 
 def new_list(request):
-    pass
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
